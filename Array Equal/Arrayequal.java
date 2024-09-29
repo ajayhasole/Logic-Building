8655e9d10f134e62eda77c2dee7974d3fabc7c0a
Ajay Hasole <hasoleajay@gmail.com>
2024-09-29 23:46:58 +0530

    Array Equal

---
+++ Array Equal/Arrayequal.java
@@ -0,0 +1,15 @@
+class Arrayequal{
+public static void main(String arg[]){
+   int arr[]={5,7,9,5};
+    int arr1[]={5,7,9,5};
+	
+	for(int i=0;i<arr.length;i++){
+	     if(arr[i]==arr1[i]){
+		 System.out.println("Array Is Equal");
+		 }else{
+		 System.out.println("Array Not Is Equal");
+		 }
+	
+	}
+}
+}
