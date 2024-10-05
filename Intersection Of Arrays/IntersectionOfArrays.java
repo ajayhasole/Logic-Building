eb8f543ee5cdfafdb08d03512b0f93f3249f8533
Ajay Hasole <hasoleajay@gmail.com>
2024-10-05 23:22:33 +0530

    Intersection Of Array

---
+++ Intersection Of Arrays/IntersectionOfArrays.java
@@ -0,0 +1,38 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class IntersectionOfArrays {
+    public static int[] intersect(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> intersection = new HashSet<>();
+
+        for (int num : nums1) {
+            set1.add(num);
+        }
+
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                intersection.add(num);
+            }
+        }
+
+        int[] result = new int[intersection.size()];
+        int index = 0;
+        for (int num : intersection) {
+            result[index++] = num;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 2, 1};
+        int[] nums2 = {2, 2};
+        int[] result = intersect(nums1, nums2);
+        
+        // Print the result
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+}
---
+++ Intersection Of Arrays/Readme.md
@@ -0,0 +1,35 @@
+Sure! Here's a simple README file with input and output descriptions for the `IntersectionOfArrays` Java program.
+
+---
+
+# Intersection of Two Arrays
+
+## Description
+This program finds the intersection of two integer arrays and returns an array of the unique elements that appear in both input arrays.
+
+## Input
+The program takes two integer arrays as input:
+- `nums1`: The first integer array.
+- `nums2`: The second integer array.
+
+### Example Input
+```java
+int[] nums1 = {1, 2, 2, 1};
+int[] nums2 = {2, 2};
+```
+
+## Output
+The program returns an array containing the unique elements that are present in both `nums1` and `nums2`.
+
+### Example Output
+For the above input, the output will be:
+```
+2 
+```
+
+### Note
+- The output array may contain fewer elements than the total number of occurrences of intersection elements, as duplicates are only included once.
+
+---
+
+Feel free to modify it according to your needs!
